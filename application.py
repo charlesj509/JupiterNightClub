16cade546b1fd124c2491a90e291ad791b532a94
charlesj509 <charlesj509@gmail.com>
2023-02-01 23:42:23 -0500

    finalchange

--- application.py
+++ application.py
@@ -1,28 +1,28 @@
 # app.py
 
 from flask import Flask, render_template
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     #images=['picture1.jpg','picture3.jpg','picture4.jpg','picture5.jpg','picture6.jpg','picture7.jpg']
     return render_template('index.html')
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/events')
+@application.route('/events')
 def events():
     return render_template('events.html')
 
-@app.route('/gallery')
+@application.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/contact')
+@application.route('/contact')
 def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
